0d0f437388b939430f266b9c9750ccdefc941a2d
Dave Lehoux <davelehoux@hotmail.com>
2018-09-23 23:35:42 -0400

    tweaks

--- src/components/Activity.vue
+++ src/components/Activity.vue
@@ -1,31 +1,54 @@
 <template>
-<section>
-  <h1>Please complete the activity details</h1>
+
+  
 
 
 <v-form v-model="valid">
-    
+  <h1>Please complete the activity details</h1>
+    <v-container>
+      <v-layout row wrap>
+         <v-flex xs12 sm6>
     <v-text-field
       v-model="activityItem.name"
       :rules="nameRules"
       :counter="20"
       label="Name"
       required
+         outline
     ></v-text-field>
      <v-text-field
       v-model="activityItem.location"
       label="Location"
       required
+       prepend-icon="place"
+         outline
     ></v-text-field>
+    
+    </v-flex> 
+  
+ <v-flex xs12 sm6>
+    <v-text-field   
+      label="Phone"
+      required
+        outline
+        append-icon="phone"
+    ></v-text-field>
+   
+    </v-flex>    
+   
+
+     <v-flex xs12 sm12>
      <v-textarea
       v-model="activityItem.description"
       label="Description"
       required
+      outline
     ></v-textarea>
-
+    </v-flex>
+   </v-layout>
+    </v-container>
 
   </v-form>
-  </section>
 </template>
 <script>
 export default {
--- src/components/RequestForm.vue
+++ src/components/RequestForm.vue
@@ -2,21 +2,29 @@
 <section>
    <h1> {{activity.name}}</h1>
     <v-chip>
-            <v-avatar class="blue">M</v-avatar>
-            {{activity.nbMaleCadets}}
+          
+           
+              <v-avatar class="blue"> {{activity.nbMaleCadets}}</v-avatar>
+              Male
           </v-chip>
-     <v-chip>
-            <v-avatar class="pink">F</v-avatar>
-            {{activity.nbFemaleCadets}}
+     <v-chip>        
+          
+               <v-avatar class="pink">{{activity.nbFemaleCadets}}</v-avatar>
+                 Female
           </v-chip>
   <v-tabs
       v-model="active"
       color="blue"
       dark
-      slider-color="yellow"
+      slider-color="lime"
     >
       <v-tab  ripple :key='1' >
-       Activity
+       Activity       
+        <v-icon
+        medium
+        color="white">
+        directions_run
+      </v-icon>
       </v-tab>
        <v-tab  ripple :key='2'>
        Rations
@@ -55,9 +63,9 @@
 </section>
 </template>
 <script>
-import Activity from '@/components/Activity';
-import Rations from '@/components/Rations';
-import Transport from '@/components/Transport';
+import Activity from "@/components/Activity";
+import Rations from "@/components/Rations";
+import Transport from "@/components/Transport";
 
 export default {
   components: { Activity, Rations, Transport },
@@ -65,15 +73,15 @@ export default {
     return {
       activity: {
         id: 1,
-        name: 'Survie',
-        description: 'This is the description',
-        startDate: '2018-02-04',
+        name: "Survie",
+        description: "This is the description",
+        startDate: "2018-02-04",
         nbMaleCadets: 10,
         nbFemaleCadets: 4,
-        location: '239 Activity Drive, Laval, QC',
+        location: "239 Activity Drive, Laval, QC"
       },
-      rations: { required: null },
+      rations: { required: null }
     };
-  },
+  }
 };
 </script>
